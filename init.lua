f959db77bcbae8d7c52ff4ba233ea7ba39a78cd0
Douglas Corbin <doug.corbin@icloud.com>
2024-05-03 16:32:54 +0100

    Set up completions.

--- init.lua
+++ init.lua
@@ -42,6 +42,96 @@ wk.register(
     { mode = 'n', prefix = '<Leader>f' }
 )
 
+------------------------- LSP CONFIG -------------------------
+local cmp = require 'cmp'
+local luasnip = require("luasnip")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+        end,
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                if luasnip.expandable() then
+                    luasnip.expand()
+                else
+                    cmp.confirm({
+                        select = true,
+                    })
+                end
+            else
+                fallback()
+            end
+        end),
+
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.locally_jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' }, -- For luasnip users.
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+    sources = cmp.config.sources({
+        { name = 'git' },
+    }, {
+        { name = 'buffer' },
+    })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    }),
+    matching = { disallow_symbol_nonprefix_matching = false }
+})
+
+-- Set up lspconfig.
 require("mason").setup()
 require("mason-lspconfig").setup()
 
@@ -50,16 +140,21 @@ require("mason-lspconfig").setup()
 -- require("lspconfig").rust_analyzer.setup {}
 
 -- Or we can use mason-lspconfig automatic server setup.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 require("mason-lspconfig").setup_handlers {
     function(server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
+        require("lspconfig")[server_name].setup {
+            capabilities = capabilities
+        }
     end,
     -- Next, you can provide a dedicated handler for specific servers.
     ["lua_ls"] = function()
         require("lspconfig").lua_ls.setup {
+            capabilities = capabilities,
             settings = { Lua = {
                 diagnostics = { globals = { "vim" } }
             } }
         }
     end
 }
+------------------------- LSP CONFIG (END) -------------------------
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,4 +63,19 @@ return {
     },
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'petertriho/cmp-git',
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = {  'rafamadriz/friendly-snippets' },
+        build = "make install_jsregexp",
+        config = function ()
+            require("luasnip.loaders.from_vscode").lazy_load()
+        end
+    },
+    'saadparwaiz1/cmp_luasnip'
 }
